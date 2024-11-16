916c0018f3ada6e20878b2a6f0524226cdfefaa4
Pritam1705 <kondapratiwarpritam855@gmail.com>
2024-11-16 12:24:18 +0530

    frontend

---
+++ build/precache-manifest.e425fc566025c2820eb3469c6be52af4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec6bf3da76cd11b6d33d",
+    "url": "/static/css/main.aa2e3fb6.chunk.css"
+  },
+  {
+    "revision": "ec6bf3da76cd11b6d33d",
+    "url": "/static/js/main.24bcd587.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7e13f6131f77da3d54b5",
+    "url": "/static/css/2.249d5a83.chunk.css"
+  },
+  {
+    "revision": "7e13f6131f77da3d54b5",
+    "url": "/static/js/2.cc0942a1.chunk.js"
+  },
+  {
+    "revision": "f24b133ed4406bb17876c3ed7fa86f22",
+    "url": "/static/media/tabler-webfont.f24b133e.eot"
+  },
+  {
+    "revision": "924bfe321562eb8cdedf675ad0e011b3",
+    "url": "/static/media/tabler-webfont.924bfe32.woff"
+  },
+  {
+    "revision": "b56d2ee6c65e0104e4ef76c027587e46",
+    "url": "/static/media/tabler-webfont.b56d2ee6.woff2"
+  },
+  {
+    "revision": "68520a6b16e40c7b3538bae7c43eb776",
+    "url": "/static/media/tabler-webfont.68520a6b.ttf"
+  },
+  {
+    "revision": "ad55e63e508f5b0e55e9d2394a2732c7",
+    "url": "/static/media/tabler-webfont.ad55e63e.svg"
+  },
+  {
+    "revision": "2b86b20fb2210bd9bd46e4d3d6082938",
+    "url": "/index.html"
+  }
+];
